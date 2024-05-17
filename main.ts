fe6c6a1bea13d976559d26deee02c8b4eba405fe
freddisgit <78456080+freddisgit@users.noreply.github.com>
2024-05-17 10:52:11 -0700

    Working on menu

--- main.ts
+++ main.ts
@@ -2,6 +2,19 @@ namespace SpriteKind {
     export const Building = SpriteKind.create()
     export const worker = SpriteKind.create()
 }
+function OpenMenu () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Building)
+    sprites.destroyAllSpritesOfKind(SpriteKind.worker)
+    scene.setBackgroundImage(assets.image`Menu`)
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (MenuOpen == 0) {
+        OpenMenu()
+    } else {
+    	
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite2,
@@ -36,156 +49,163 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+function SpawnStuff () {
+    info.setScore(0)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . b b . . 
+        . . . . . . . . . . . . . b b . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `, SpriteKind.Building)
+    worker = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 5 . . . . . . . . . . 
+        . . . . 5 1 5 b b b . . . . . . 
+        . . . . f d f . e . . . . . . . 
+        . . . 6 6 6 6 6 e . . . . . . . 
+        . . . . . 6 . . e . . 8 b . . . 
+        . . . . f f f . . . 8 8 b . . . 
+        . . . . f . f . . . b 8 b . . . 
+        `, SpriteKind.worker)
+    mySprite2 = sprites.create(img`
+        . . . . f . . . . . . . 
+        . . . f 1 f . . . . . . 
+        . . . f 1 f . . . . . . 
+        f f . f 1 f . f f f f . 
+        f 1 f f 1 f f 1 f 1 f . 
+        f 1 1 1 1 1 1 1 1 1 f . 
+        f 1 1 f 1 f 1 f 1 1 f . 
+        f 1 1 f 1 f 1 f 1 1 f . 
+        f 1 1 1 1 1 1 1 1 f . . 
+        . f 1 1 1 1 1 1 f . . . 
+        . f 1 1 f 1 f f . . . . 
+        . . f f . f . . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite2, 100, 100)
+    scene.setBackgroundColor(10)
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . b b . . 
+        . . . . . . . . . . . . . b b . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `,img`
+        . . . . . . . . . . . . . . b b 
+        . . . . . . . . . . . . . b b . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `,img`
+        . . . . . . . . . . . . . b . . 
+        . . . . . . . . . . . . . . b . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `,img`
+        . . . . . . . . . . . . b . . . 
+        . . . . . . . . . . . . . b . . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `,img`
+        . . . . . . . . . . . . b . b b 
+        . . . . . . . . . . . . . b . . 
+        . . . . . . . . . . . f f f f f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e f e f 
+        . . . . . . . . . . . f e e e f 
+        . . . . . f f f f . . f e e e f 
+        f f f f f f e e f f f f e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e f f f e e e e e e e e e f 
+        f e e b f b e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f e e e e e e e e e e e e e e f 
+        f f f f f f f f f f f f f f f f 
+        `],
+    200,
+    true
+    )
+    worker.x = 10
+    mySprite.x = 25
+    MenuOpen = 0
+}
 info.onScore(10000, function () {
     game.gameOver(true)
 })
+let worker: Sprite = null
+let mySprite: Sprite = null
 let mySprite2: Sprite = null
-info.setScore(0)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . b b . . 
-    . . . . . . . . . . . . . b b . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `, SpriteKind.Building)
-let worker = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 5 . . . . . . . . . . 
-    . . . . 5 1 5 b b b . . . . . . 
-    . . . . f d f . e . . . . . . . 
-    . . . 6 6 6 6 6 e . . . . . . . 
-    . . . . . 6 . . e . . 8 b . . . 
-    . . . . f f f . . . 8 8 b . . . 
-    . . . . f . f . . . b 8 b . . . 
-    `, SpriteKind.worker)
-mySprite2 = sprites.create(img`
-    . . . . f . . . . . . . 
-    . . . f 1 f . . . . . . 
-    . . . f 1 f . . . . . . 
-    f f . f 1 f . f f f f . 
-    f 1 f f 1 f f 1 f 1 f . 
-    f 1 1 1 1 1 1 1 1 1 f . 
-    f 1 1 f 1 f 1 f 1 1 f . 
-    f 1 1 f 1 f 1 f 1 1 f . 
-    f 1 1 1 1 1 1 1 1 f . . 
-    . f 1 1 1 1 1 1 f . . . 
-    . f 1 1 f 1 f f . . . . 
-    . . f f . f . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite2, 100, 100)
-scene.setBackgroundColor(10)
-animation.runImageAnimation(
-mySprite,
-[img`
-    . . . . . . . . . . . . b b . . 
-    . . . . . . . . . . . . . b b . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `,img`
-    . . . . . . . . . . . . . . b b 
-    . . . . . . . . . . . . . b b . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `,img`
-    . . . . . . . . . . . . . b . . 
-    . . . . . . . . . . . . . . b . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `,img`
-    . . . . . . . . . . . . b . . . 
-    . . . . . . . . . . . . . b . . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `,img`
-    . . . . . . . . . . . . b . b b 
-    . . . . . . . . . . . . . b . . 
-    . . . . . . . . . . . f f f f f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e f e f 
-    . . . . . . . . . . . f e e e f 
-    . . . . . f f f f . . f e e e f 
-    f f f f f f e e f f f f e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e f f f e e e e e e e e e f 
-    f e e b f b e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f e e e e e e e e e e e e e e f 
-    f f f f f f f f f f f f f f f f 
-    `],
-200,
-true
-)
-worker.x = 10
-mySprite.x = 25
+let MenuOpen = 0
+SpawnStuff()
 game.onUpdateInterval(2000, function () {
     animation.runImageAnimation(
     worker,
